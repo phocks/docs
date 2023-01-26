fd56ad5d81d072c32767e1f9dfdf74e618391fdb
Joshua Byrd <phocks@gmail.com>
2023-01-27 09:54:56 +1000

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>phocks docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/phocks/nextra-personal-docs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/f6TUahfpdJ',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/phocks/nextra-personal-docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'a byrd house joint',
   },
 }
 
