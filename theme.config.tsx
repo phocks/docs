07a546de979c608e958341f57cc35f1ef9c8b514
Joshua Byrd <phocks@gmail.com>
2023-02-13 19:06:29 +1000

    Current date

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>JBDoc</span>,
   project: {
-    link: 'https://github.com/phocks',
+    link: "https://github.com/phocks",
   },
   // chat: {
   //   link: 'https://discord.gg/f6TUahfpdJ',
   // },
-  docsRepositoryBase: 'https://github.com/phocks/docs/tree/main/',
+  docsRepositoryBase: "https://github.com/phocks/docs/tree/main/",
   footer: {
-    text: 'a byrd house joint',
+    text: `© Joshua Byrd. ${new Date().getFullYear()}.`,
   },
-}
+};
 
-export default config
+export default config;
